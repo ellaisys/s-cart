41b416b2a9b802637beee5ad4bb16ff802496f40
Naruto <lanhktc@gmail.com>
2020-02-17 18:39:44 +0700

    no message

--- database/migrations/2020_00_00_step2_create_tables_shop.php
+++ database/migrations/2020_00_00_step2_create_tables_shop.php
@@ -434,7 +434,7 @@ class CreateTablesShop extends Migration
         });
 
         Schema::create(SC_DB_PREFIX.'shop_sessions', function ($table) {
-            $table->string('id')->unique();
+            $table->string('id',100)->unique();
             $table->unsignedInteger('user_id')->nullable();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
